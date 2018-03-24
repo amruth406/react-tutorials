89b0b72e796a2ab578d3e5f589a539428b211cd9
amruth406 <amruth406@gmail.com>
2018-03-24 16:07:07 +0530

    ''

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+setInterval(function(){
+
 ReactDOM.render(
-<h1> welcome amruth</h1>,
+<h1>Time is: {new Date().toLocaleTimeString()}</h1>,
 document.getElementById('root')
 );
+},1000);
